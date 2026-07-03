9757d52a588f988f36725f7a997b74e45eadb514
Bismah <bismahnasir000@gmail.com>
2026-07-03 08:44:41 +0500

    feat: implement Services page and configure application routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar.jsx";
 import Home from "./components/Home/Home.jsx";
 import About from "./components/About/About.jsx";
+import ServicesPage from "./components/Services/ServicesPage.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 
 function App() {
@@ -15,16 +16,7 @@ function App() {
 
                 <Route path="/about" element={<About />} />
 
-                <Route
-                    path="/services"
-                    element={
-                        <main className="min-h-screen bg-background pt-20">
-                            <h1 className="text-center text-4xl font-bold text-text-primary">
-                                Services Page
-                            </h1>
-                        </main>
-                    }
-                />
+                <Route path="/services" element={<ServicesPage />} />
 
                 <Route
                     path="/portfolio"
